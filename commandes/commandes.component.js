ea5f37d5a45ee298e8c31fbfdb0d8cc7ef3fd156
Jimmy Sené <jimmy.sene@laposte.net>
2017-10-10 16:17:08 +0200

    Liaison de l'appli avec Elasticsearch en envoie et recup.

--- commandes/commandes.component.js
+++ commandes/commandes.component.js
@@ -3,14 +3,22 @@ angular.module('commandesList').component('commandesList', {
   controller: function CommandesList($http) {
 
     this.list = [];
-    var self = this;
 
-    $http.get("data.json").then(function(response) {
-      self.list = response.data;
+    var self = this;
+    var args = '{"query":{"match_all" :{ }}}';
+
+    $http.post("http://10.213.93.75:9200/angulartest/commandes/_search", args).then(function(response) {
+      for(var i = 0 ; i < response.data['hits']['total']; i++)
+        {
+          self.list.push(response.data['hits']['hits'][i]['_source']);
+        }
     });
 
     this.addCommande = function() {
-      this.list.push({name: this.name, desc: this.desc});
+        var args2 = '{"name":"' + this.name + '","desc":"'+ this.desc +'"}';
+        console.log(args2);
+      var commande = this.list.push({name: this.name, desc: this.desc});
+      $http.post('http://10.213.93.75:9200/angulartest/commandes', args2);
     };
   }
 });
